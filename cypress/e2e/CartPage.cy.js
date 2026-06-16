31e78a3aefe76afd040fb2795d9c2bdcd973607e
ShairaSaiyara <shaira.islam@enosisbd.com>
2026-06-17 01:48:26 +0600

    Removed some files and created one file for Cart related tests

--- cypress/e2e/CartPage.cy.js
+++ cypress/e2e/CartPage.cy.js
@@ -1,21 +1,33 @@
-import LoginPage from '../../pages/LoginPage';
-import InventoryPage from '../../pages/InventoryPage';
+import LoginPage from '../pages/LoginPage';
+import InventoryPage from '../pages/InventoryPage';
 
-describe('Remove From Cart', () => {
+describe('Cart Page Tests', () => {
 
     const loginPage = LoginPage;
     const inventoryPage = new InventoryPage();
 
     beforeEach(() => {
         cy.fixture('users').then((user) => {
-
             cy.visit('/');
-
             loginPage.enterUsername(user.validUser.username);
             loginPage.enterPassword(user.validUser.password);
             loginPage.clickLogin();
         });
     });
+
+    it('should add backpack to cart', () => {
+
+        inventoryPage.addBackpackToCart();
+
+        cy.get('.shopping_cart_badge').should('contain', '1');
+    });
+
+    it('should update count on cart icon accurately', () => {
+        inventoryPage.addBackpackToCart();
+        inventoryPage.addBikeLightToCart();
+
+        cy.get('.shopping_cart_badge').should('contain', '2');
+    })
 
     it('should remove product from cart', () => {
 
@@ -23,7 +35,5 @@ describe('Remove From Cart', () => {
         inventoryPage.removeBackpackFromCart();
 
         cy.get('.shopping_cart_badge').should('not.exist');
-        // cy.get('.cart_list').should('be.empty')
     });
-
 });
