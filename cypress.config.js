32b7a8c971ad975ee5de1bd6584c16c5e2f5fb45
ShairaSaiyara <shaira.islam@enosisbd.com>
2026-08-09 01:33:55 +0600

    Added reporting and logging with mochawesome and cypress-terminal-report

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,26 @@
 const { defineConfig } = require("cypress");
-
+const installLogsPrinter = require('cypress-terminal-report/src/installLogsPrinter');
 
 module.exports = defineConfig({
+  reporter: 'mochawesome',
+  reporterOptions: {
+    reportDir: 'cypress/reports',
+    overwrite: false,
+    html: false,
+    json: true
+  },
   e2e: {
     baseUrl: "https://www.saucedemo.com",
     setupNodeEvents(on, config) {
+      installLogsPrinter(on, {
+        printLogsToConsole: 'always',
+        printLogsToFile: 'always',
+        outputRoot: 'cypress/logs/',
+        outputTarget: {
+          'out.txt': 'txt',
+          'out.json': 'json',
+        }
+      });
     }
   }
 });
